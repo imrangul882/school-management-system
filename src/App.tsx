cd24f08a5a439783e2b73e2d3e0fd9ceb18d4f03
imrangul882 <imranshekhsiddiquee12@gmail.com>
2026-09-14 12:39:42 +0500

    fix App.tsx and css imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './app.css';
+import './App.css';
 import { useState, useEffect } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { StudentPortal } from './components/StudentPortal';
